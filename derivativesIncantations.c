b155b2af673b8d4a64faebf8f98e3ec683a24754
kamillamamatova <kamillamamatova.km@gmail.com>
2025-01-30 22:06:44 -0500

    Update derivativesIncantations.c

--- derivativesIncantations.c
+++ derivativesIncantations.c
@@ -11,3 +11,37 @@ int isDerivative(char * a, char * b){
   return strstr(b, a) != NULL;
 }
 
+int main(){
+  char incantations[MAX_INCANTATIONS][MAX_LENGTH];
+  int isNonDerivative[MAX_INCANTATIONS] = {0}; // 0 means non-derivative
+  int incantationCount = 0;
+
+  // Read incantations until "END" is encountered
+  while(1){
+    char buffer[MAX_LENGTH];
+    if(!fgets(buffer, MAX_LENGTH, stdin)) break;
+
+    // Remove newline character if present
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    // Stop reading if we encounter "END"
+    if(strcmp(buffer, "END")] == 0) break;
+
+    // Copy the incantations to the array
+    strcpy(incantations[incantationCount++], buffer);
+  }
+
+  // Mark derivatives by looping through all pairs
+  for(int i = 0; i < incantationCount; i++){
+    for(int j = 0; j < incantationCount; j++){
+      if(i == j) continue;
+
+      // Check if incantation[j] is a derivative of incantation[i]
+      if(isDerivative(incantations[i], incantations[j])){
+        // Mark incantation[j] as derivative
+        isNonDerivative[j] = 1; 
+      }
+    }
+  }
+  
+
